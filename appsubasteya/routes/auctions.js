07c5f5e71a09b2b1f959cbe56df0159a0619a082
ramiro <ramiroandreshg@gmail.com>
2019-06-27 19:40:43 -0300

    wip

--- appsubasteya/routes/auctions.js
+++ appsubasteya/routes/auctions.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express.Router();
-const fs = require('fs');
-const path = require('path');
-
-const auction_scripts = fs.readFileSync(path.join(__dirname + '/../', `public/javascripts/auction-scripts.js`)).toString();
 
 /* GET auction page. */
 app.get('/', function(req, res) {
-  res.render('auctions', { title: 'Auctions Page', auction_scripts: auction_scripts});
+  res.render('auctions', { title: 'Auctions Page'});
 });
 
 app.post('/start', function (req, res) {
