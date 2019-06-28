63e8bf969d18e623b25ad5062721ac385aa12dc9
ramiro <ramiroandreshg@gmail.com>
2019-06-28 11:36:37 -0300

    thoughts

--- appsubasteya/routes/auctions.js
+++ appsubasteya/routes/auctions.js
@@ -3,7 +3,24 @@ const app = express.Router();
 
 /* GET auction page. */
 app.get('/', function(req, res) {
+  res.send('OK');
+
+  /*
+    web3 logic to fech all auctions from current contract and reeturn them as a json
+    Algo we should make the current best bid always VISIBLE
+  */
+});
+
+app.get('/new', function(req, res) {
   res.render('auctions', { title: 'Auctions Page'});
+});
+
+app.get('/bid', function(req, res) {
+  res.send('OK');
+
+  /*
+    web3 logic to send a bid to the current auction
+  */
 });
 
 app.post('/start', function (req, res) {
