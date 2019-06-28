a09128068c531c5c41ec391a32b7f39fe8ca23e2
ramiro <ramiroandreshg@gmail.com>
2019-06-28 19:58:31 -0300

    contract-adv

--- Subasteya.sol
+++ Subasteya.sol
@@ -13,9 +13,11 @@ contract Subasteya {
   uint256 private maxPrice;
   uint256 private maxOffers;
 
+  bool private showPrices;
+
   Bid[] private bids;
 
-  bool private auctionOpen;
+  AuctionState private state;
 
   /*  STRUCTS */
   struct Bid {
@@ -23,11 +25,14 @@ contract Subasteya {
     uint256 amount;
   }
 
+  /*  ENUMS */
+  enum AuctionState { JustCreated, Open, Finished }
+
   /*  EVENTS */
 
   /*  CONSTRUCTOR */
-  constructor(bytes32 itmUrl, bytes32 itmName, bytes32 itmDesc,
-    uint256 itmBPrice, uint256 itmPmin, uint256 itmPmax, uint256 itmMaxOffers) public {
+  constructor(bytes32 itmUrl, bytes32 itmName, bytes32 itmDesc, uint256 itmBPrice,
+    uint256 itmPmin, uint256 itmPmax, uint256 itmMaxOffers, bool publicPrices) public {
     owner = msg.sender;
     url = itmUrl;
     name = itmName;
@@ -36,32 +41,93 @@ contract Subasteya {
     minPrice = itmPmin;
     maxPrice = itmPmax;
     maxOffers = itmMaxOffers;
-    auctionOpen = true;
+    showPrices = publicPrices;
+    state = AuctionState.JustCreated;
   }
 
   /*  MODIFIERS */
+  modifier onlyOwner () {
+    require(msg.sender == owner, "onlyOwner - caller is not the owner.");
+    _;
+  }
+
+  modifier onlyOpen () {
+    require(state == AuctionState.Open, "onlyOpen - auction is not open anymore");
+    _;
+  }
+
+  modifier justCreated () {
+    require(state == AuctionState.JustCreated, "justCreated - Cant open auction twice");
+    _;
+  }
 
   /*  PAYABLE FUNCTION */
   function() external payable{}
 
   /*  EXTERNAL METHODS */
-  function bid () external {}
+  function openAuction () external onlyOwner justCreated {
+    state = AuctionState.Open;
+  }
 
-  function listBids () external {}
+  function bid () external onlyOpen {}
 
-  function closeAuction () external {}
+  // function listBids () external activeContract() {}
 
-  function getAuctionInfo () external {}
+  function getBid (uint pos) external onlyOpen view returns(address bidder, uint256 amount) {
+    require(pos < getBidsCount(), "The bid requested does not exist yet");
+    
+    return (bids[pos].bidder, bids[pos].amount);
+  }
 
-  function getCurrentMaxBid () external {}
+  function closeAuction () external onlyOpen {}
 
-  /*  INTERNAL METHODS */
-  function disableContract () internal {}
+  function getAuctionInfo () external view onlyOpen returns(bytes32 itmUrl, bytes32 itmName, bytes32 itmDesc, uint256 itmBPrice,
+    int256 itmPmin, int256 itmPmax, int256 itmMaxOffers){
+    int256 pMin = int(minPrice);
+    int256 pMax = int(maxPrice);
+    int256 mOffers = int(maxOffers);
+    
+    if (showPrices) {
+     pMin = -1;
+     pMax = -1;
+     mOffers = -1;
+    }
 
-  function makePayment () internal {}
+    return (url, name, description, basePrice, pMin, pMax, mOffers);
+  }
 
-  function returnMoney () internal {}
+  function getCurrentMaxBid () external onlyOpen view returns(address bidder, uint256 amount) {
+    uint256 bidsCount = getBidsCount();
+    require(bidsCount > 0, "No offers yet");
+    uint256 lastIdx = bidsCount - 1;
+
+    return (bids[lastIdx].bidder, bids[lastIdx].amount);
+  }
+
+  /*  PRIVATE METHODS */
+  function disableContract () private {
+    state = AuctionState.Finished;
+  }
+
+  function checkIfAuctionEnds () private view returns(bool) {
+    return lastBidCoversMaxPrice() || maxOffersReached();
+  }
+
+  function makeThePayment () private {}
+
+  function refundPreviousBestBid () private {}
 
   /*  AUX FUNCTIONS */
+  function getBidsCount () public onlyOpen view returns(uint256 bidsCount) {
+    return bids.length;
+  }
+
+  function lastBidCoversMaxPrice () private view returns(bool) {
+    return bids[getBidsCount() - 1].amount >= maxPrice;
+  }
+
+  function maxOffersReached () private view returns(bool) {
+    return getBidsCount() >= maxOffers;
+  }
 
 }
