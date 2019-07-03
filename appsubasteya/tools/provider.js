bef088e28196d3e0c159f9a623a8793c3a458e1f
Ramiro <ramiroandreshg@gmail.com>
2019-07-03 09:45:53 -0300

    connectedToGanache

--- appsubasteya/tools/provider.js
+++ appsubasteya/tools/provider.js
@@ -1,13 +1,16 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
 
 exports = module.exports = {};
 
 const mnemonic = 'unveil dynamic pride someone local since quantum buffalo trick swear quit stay';
 const testnet = 'https://rinkeby.infura.io/v3/969c6e3184564fa68c449771085e8ef1';
 
-const _provider = new HDWalletProvider(
+let _provider = new HDWalletProvider(
     mnemonic,
     testnet
 );
+
+_provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:8545')
 
 exports.HDWalletProvider = _provider;
