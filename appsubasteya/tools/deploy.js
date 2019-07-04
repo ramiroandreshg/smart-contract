25da405e96f35ed45f5b2b356690ed93569dfb7e
ramiro <ramiroandreshg@gmail.com>
2019-07-04 00:29:59 -0300

    ref

--- appsubasteya/tools/deploy.js
+++ appsubasteya/tools/deploy.js
@@ -8,8 +8,6 @@ const _bytecode = '0x' + bytecode.bytecode.object;
 
 const _abi = require('./../contracts/abi.json');
 
-// rinkeby endpoint: rinkeby.infura.io/v3/969c6e3184564fa68c449771085e8ef1
-
 exports.deploy = async (account, params) => {
   const web3 = new Web3(provider.HDWalletProvider);
   const result = await new web3.eth.Contract(_abi)
@@ -23,7 +21,7 @@ exports.deploy = async (account, params) => {
     value: 0
   });
 
-  console.log('address', result.options.address);
+  console.log('contract address', result.options.address);
   
   return result.options.address;
 }
