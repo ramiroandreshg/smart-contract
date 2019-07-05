28b5e3fd23b14fc94d0bf70095fc8110a724ba82
Ramiro <ramiroandreshg@gmail.com>
2019-07-05 17:58:12 -0300

    wip

--- api/routes/auctions.js
+++ api/routes/auctions.js
@@ -2,77 +2,28 @@ const express = require('express');
 const i = require('./../tools/internals');
 
 const app = express.Router();
-let deployedContractAddress;
+const currentContracts = [];
 
-app.get('/new', function(req, res) {
-  res.render('auctions', { title: 'Auctions Page'});
+app.get('/', function(req, res) {
+  try {
+    i.createAuctionAndDeployContract('args');
+  } catch (e) {
+    console.log('ERRORRRR', e.message);
+  }
+  res.json(currentContracts);
 });
 
-app.post('/start', async function (req, res) {
+app.post('/', async function(req, res) {
   const args = req.body;
 
-  const output = {};
   try {
-    deployedContractAddress = await i.deployContract(args);
-    output.success = true;
-    output.auctionAddress = deployedContractAddress;
+    auction = await i.createAuctionAndDeployContract(args);
   } catch (err) {
-    console.log('Auction Start ERROR -> ', err);
-    output.success = false;
-    output.error = err.message;
+    // handle it
   }
-  
-  res.json(output);
-});
 
-app.post('/end', async function (req, res) {
-  const args = req.body;
-  
-  const output = {};
-  try {
-    await i.closeAuction(deployedContractAddress, args);
-    output.success = true;
-  } catch (err) {
-    console.log('End Auction ERROR -> ', err.message);
-    output.success = false;
-    output.error = err.message;
-  }
-  
-  res.json(output);
-});
+  return res.json({'out': 'put'});
 
-app.get('/bids', async function(req, res) {
-  let output = {};
-  try {
-    const bidList = await i.getAllBids(deployedContractAddress);
-    output.bids = bidList;
-  } catch (err) {
-    console.log('Bid List Start ERROR -> ', err.message);
-    output.error = err.message;
-  }
-  
-  res.json(output);
-});
-
-app.get('/bid', function(req, res) {
-  res.render('bid', { title: 'Bid Page'});
-});
-
-app.post('/bid', async function(req, res) {
-  const args = req.body;
-
-  const output = {};
-  try {
-    await i.placeBid(deployedContractAddress, args);
-    output.success = true;
-    output.bidAmount = args.amount;
-  } catch (err) {
-    console.log('Place Bid ERROR -> ', err.message);
-    output.success = false;
-    output.error = err.message;
-  }
-  
-  res.json(output);
 });
 
 module.exports = app;
--- api/tools/internals.js
+++ api/tools/internals.js
@@ -1,98 +1,16 @@
-const Web3 = require('web3');
-const d = require('./deploy');
-const provider = require('./provider');
-
-const web3 = new Web3(provider.HDWalletProvider);
+const AppError = require('./apperror');
 
 exports = module.exports = {};
 
-exports.deployContract = async function (args) {
-  let owner;
-  if (!args.ownerAddress) {
-    const accounts = await web3.eth.getAccounts();
-    owner = accounts[0];
-  } else {
-    owner = args.ownerAddress;
-    delete args.ownerAddress;
+exports.createAuctionAndDeployContract = function (auction) {
+  throw new AppError('app error', 404, "further explanation", true)
+
+  if (!_validAuction(auction)) {
+    throw new Error('invalid auction');
   }
 
-  args.publicInfo = args.publicInfo === 'hide' ? true : false;
-  const params = Object.values(args);
-  const contractAddress = await d.deploy(owner, params);
-
-  await _openAuction(owner, contractAddress);
-
-  return contractAddress;
 };
 
-async function _openAuction (owner, contractAddress) {
-  const contractInstance = new web3.eth.Contract(d.abi, contractAddress);
-
-  return contractInstance.methods.openAuction().send({
-    gas: '3000000',
-    from: owner
-  });
-};
-
-exports.getAllBids = async function (contractAddress) {
-  if (!contractAddress) {
-    throw new Error('undefined contract address');
-  }
-
-  const contractInstance = new web3.eth.Contract(d.abi, contractAddress);
-
-  const bidsCount = await contractInstance.methods.getBidsCount().call();
-  let bidList = [];
-  let bid;
- 
-  if (bidsCount > 0) {
-    for (let i = 1; i <= bidsCount; i++) { 
-      try {
-        bid = await _getBid(i, contractInstance);
-        bidList.push(bid);
-      } catch (err) {
-        console.log('Unable to fetch bid number #', i);
-        console.log('error: ', err);
-      }
-    } 
-  }
-
-  return bidList;
-};
-
-async function _getBid(bidNumber, contractInstance) {
-  const bid = await contractInstance.methods.getBid(bidNumber).call();
-  
-  return {
-    bidNumber,
-    bidderAddress: bid.bidder,
-    amount: bid.amount
-  }
-};
-
-exports.placeBid = async function (contractAddress, args) {
-  if (!contractAddress) {
-    throw new Error('undefined contract address');
-  }
-
-  const contractInstance = new web3.eth.Contract(d.abi, contractAddress);
-
-  const bidder = args.address;
-  const amount = args.amount;
-
-  await contractInstance.methods.bid().send({
-    gas: '3000000',
-    from: bidder,
-    value: amount
-  });
-};
-
-exports.closeAuction = async function (contractAddress, args) {
-  const owner = args.owner;
-  
-  const contractInstance = new web3.eth.Contract(d.abi, contractAddress);
-  await contractInstance.methods.closeAuction().send({
-    gas: '3000000',
-    from: owner
-  });
-};
+function _validAuction (auction) {
+  return true;
+}
