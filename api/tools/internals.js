6a77a06747f7ec5ea5eb48414c377654ed7c9f4f
Ramiro <ramiroandreshg@gmail.com>
2019-07-05 18:09:13 -0300

    wip

--- api/tools/internals.js
+++ api/tools/internals.js
@@ -4,7 +4,7 @@ exports = module.exports = {};
 
 exports.createAuctionAndDeployContract = function (auction) {
   if (!_validAuction(auction)) {
-    throw new AppError('invalid auction', 'Bad Request', 400);
+    throw new AppError('invalid auction', 'Bad Request', 400); // http code 422 could be more descriptive here
   }
 
 };
